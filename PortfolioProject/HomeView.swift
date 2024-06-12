b9b880442cc074ab20302244ffc9aa77b3183695
tomisin <tomisin18@gmail.com>
2024-06-12 12:11:16 +0100

    Home page UI fixes

--- PortfolioProject/HomeView.swift
+++ PortfolioProject/HomeView.swift
@@ -15,25 +15,44 @@ struct HomeView: View {
         
         ScrollView {
             
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 30) {
                 
-                Text("About Me")
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                    Text("About Me")
+                        .font(.callout)
+                        .bold()
+                    
+                    Text("""
+                         iOS developer with about 5 years of experience building mobile application. Has a track record of successful
+                         projects across finance & payments, retail, and health from concept to the AppStore and proven expertise in
+                         agile development processes. Led an iOS team to develop financial solution processing over $90 million for over
+                         200,000 users.
+                         """)
+                    .font(.caption)
+                    .lineSpacing(15.0)
+                }
+                .padding(.horizontal)
                 
-                Text("""
-                     iOS developer with about 5 years of experience building mobile application. Has a track record of successful
-                     projects across finance & payments, retail, and health from concept to the AppStore and proven expertise in
-                     agile development processes. Led an iOS team to develop financial solution processing over $90 million for over
-                     200,000 users.
-                     """)
-                .font(.caption)
-                .lineSpacing(10.0)
-                .padding()
-                
-                
-                
-                Text("Work History")
-                WorkHistorySlider()
-                
+                VStack {
+                    HStack {
+                        Text("Work History")
+                            .font(.callout)
+                            .bold()
+                        
+                        Spacer()
+                        
+                        Button(action:  {} ) {
+                            Text("View all")
+                                .font(.footnote)
+                                .bold()
+                                .foregroundStyle(.orange)
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    WorkHistorySlider()
+                }
             }
         }
     }
